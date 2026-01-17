336c45c0eea1c74b78afcc00bce74ca8f3eb3175
giglzghost <giglzghost@gmail.com>
2026-01-17 17:59:57 -0600

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
-
 app.use(express.json());
 
-app.get('/', (req, res) => res.send('Prime Forge v1'));
-app.get('/prime', (req, res) => res.send('Prime module stub'));
-app.get('/forge', (req, res) => res.send('Forge endpoint ready'));
-app.post('/forge', (req, res) => res.json({ forged: true }));
+app.all('*', (req, res) => {
+  const path = req.path.slice(1) || 'root';
+  if (path === 'prime') return res.send('Prime ready');
+  if (path === 'forge') return res.send('Forge ready');
+  res.send('Prime Forge API v1');
+});
 
 module.exports = app;
