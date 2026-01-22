79c4118afe550aa56179e21ca510e604b140f8b0
giglzghost <giglzghost@gmail.com>
2026-01-21 22:03:37 -0600

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,12 +1,1 @@
-const router = require('./utils/allrouter.js');  // ./utils since same repo
-
-module.exports = async (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  
-  try {
-    const result = await router.handlePrimeForgeRequest(req);
-    res.status(200).end(JSON.stringify(result));
-  } catch (e) {
-    res.status(500).end(JSON.stringify({error: e.message}));
-  }
-};
+const router = require('./utils/allrouter
