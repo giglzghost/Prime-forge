36a85ded1885e055044bc536beec95c066a44bb3
giglzghost <giglzghost@gmail.com>
2026-01-22 23:19:08 -0600

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: { moonshadow: '#1a0033' }
+    }
+  },
+  plugins: [],
+};
